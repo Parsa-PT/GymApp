0def8a9ceae0c5285e04d98ed3ee169005a534dc
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2024-08-13 20:21:10 +0330

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,39 @@
+export const IMAGE_SLIDER = [
+    require('../assets/images/slide1.png'),
+    require('../assets/images/slide2.png'),
+    require('../assets/images/slide3.png'),
+    require('../assets/images/slide4.png'),
+    require('../assets/images/slide5.png'),
+]
+
+
+export const BODY = [
+    {
+        title:'back',
+        pic : require('../assets/images/back.png')
+    },
+    {
+        title:'cario',
+        pic : require('../assets/images/cardio.png')
+    },
+    {
+        title:'lower arms',
+        pic : require('../assets/images/lowerArms.png')
+    },
+    {
+        title:'lower legs',
+        pic : require('../assets/images/lowerLegs.png')
+    },
+    {
+        title:'chest',
+        pic : require('../assets/images/chest.png')
+    },
+    {
+        title:'waist',
+        pic : require('../assets/images/waist.png')
+    },
+    {
+        title:'neck',
+        pic : require('../assets/images/neck.png')
+    },
+]
